22f05f8ea9a61a3f19a1fbaf45f22ba9cf72e019
nima-abdpoor <nimaabdi328@yahoo.com>
2021-04-06 09:44:13 +0430

    bluetooth permission and enabling bluetooth

--- app/src/main/java/com/nima/bluetoothchatapp/MainActivity.kt
+++ app/src/main/java/com/nima/bluetoothchatapp/MainActivity.kt
@@ -1,11 +1,64 @@
 package com.nima.bluetoothchatapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.bluetooth.BluetoothAdapter
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+    private val PERMISSION_CODE = 100
+    private val REQUEST_ENABLE_BT = 101
+    private var bluetoothAdapter: BluetoothAdapter? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        checkForPermission()
+        setBluetoothAdapter()
+        enableBluetooth()
+    }
+
+    private fun enableBluetooth() {
+        if (bluetoothAdapter?.isEnabled == false) {
+            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
+        }
+    }
+
+    private fun setBluetoothAdapter() {
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+        if (bluetoothAdapter == null) {
+            Toast.makeText(this,"the device doesn't support Bluetooth!",Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun checkForPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            if (ContextCompat.checkSelfPermission(baseContext,
+                            Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(
+                        this,
+                        arrayOf(Manifest.permission.ACCESS_BACKGROUND_LOCATION),
+                        PERMISSION_CODE)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when(requestCode){
+            REQUEST_ENABLE_BT ->{
+                when(resultCode){
+                    RESULT_OK -> {}
+                    RESULT_CANCELED -> {finish()}
+                }
+            }
+        }
     }
 }
