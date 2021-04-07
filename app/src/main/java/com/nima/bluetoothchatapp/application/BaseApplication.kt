e9fb8821b407528ee4cf0c63410db7448f7b9e27
nima-abdpoor <nimaabdi328@yahoo.com>
2021-04-07 21:44:39 +0430

    add BaseApplication.kt for Hilt Settings

---
+++ app/src/main/java/com/nima/bluetoothchatapp/application/BaseApplication.kt
@@ -0,0 +1,8 @@
+package com.nima.bluetoothchatapp.application
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BaseApplication :Application(){
+}
