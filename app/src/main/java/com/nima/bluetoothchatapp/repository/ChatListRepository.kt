9fd33700373145f15660883ed5ab90fd8f5a3b3d
nima-abdpoor <nimaabdi328@yahoo.com>
2021-04-09 16:43:04 +0430

    get ConnectedDevices from repository to viewModel.

--- app/src/main/java/com/nima/bluetoothchatapp/repository/ChatListRepository.kt
+++ app/src/main/java/com/nima/bluetoothchatapp/repository/ChatListRepository.kt
@@ -1,15 +1,23 @@
 package com.nima.bluetoothchatapp.repository
 
 import com.nima.bluetoothchatapp.database.MyDao
-import com.nima.bluetoothchatapp.mapper.ChatMessageMapper
+import com.nima.bluetoothchatapp.devices.BLDevice
+import com.nima.bluetoothchatapp.mapper.ChatListMapper
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.count
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class ChatListRepository @Inject constructor(private val dao: MyDao) {
 
     @Inject
-    lateinit var chatLisMapper: ChatMessageMapper
+    lateinit var chatLisMapper: ChatListMapper
 
-    fun getConnectedDevices(){
-        dao.getConnectedDevices()
+    suspend fun getConnectedDevices() : Flow<BLDevice>? {
+        val connectedDevices = dao.getConnectedDevices()
+        if (connectedDevices.count() != 0){
+            return connectedDevices.map { chatLisMapper.mapFromEntity(it)  }
+        }
+        return null
     }
 }
--- app/src/main/java/com/nima/bluetoothchatapp/viewmodel/ChatListViewModel.kt
+++ app/src/main/java/com/nima/bluetoothchatapp/viewmodel/ChatListViewModel.kt
@@ -1,10 +1,23 @@
 package com.nima.bluetoothchatapp.viewmodel
 
 import androidx.hilt.lifecycle.ViewModelInject
+import com.nima.bluetoothchatapp.devices.BLDevice
 import com.nima.bluetoothchatapp.repository.ChatListRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.launch
 
 class ChatListViewModel @ViewModelInject constructor(
     private val repository: ChatListRepository
 ) {
+    private var allDevices: List<BLDevice?>? = null
 
+    fun getConnectedDevices(): List<BLDevice?>?{
+        CoroutineScope(Dispatchers.IO).launch {
+            allDevices =
+                repository.getConnectedDevices()?.toList()
+        }
+        return allDevices
+    }
 }
